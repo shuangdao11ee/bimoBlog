8e36d9b145175889c19524ccb470b60652b0b707
iAmBimo <shuangdao11ee@outlook.com>
2024-05-05 21:55:40 +0800

    feat: 增加react router

--- apps/blog/src/main.tsx
+++ apps/blog/src/main.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+]);
 
 const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
