45a62ca50d7da43e0ff1a56709211cabb4e52ffd
iAmBimo <shuangdao11ee@outlook.com>
2024-05-12 12:28:13 +0800

    feat: start 脚本传入 webpackdevServer config 配置

--- apps/blog/script/start.js
+++ apps/blog/script/start.js
@@ -7,7 +7,7 @@ var config = require('../webpack.config');
 
 var compiler = webpack(config);
 
-var devServer = new webpackDevServer({ host: '0.0.0.0', port: 8080 }, compiler);
+var devServer = new webpackDevServer(config.devServer, compiler);
 devServer.startCallback(() => {
   console.log('[dev] ready to start dev');
 });
--- apps/blog/webpack.config.js
+++ apps/blog/webpack.config.js
@@ -77,6 +77,8 @@ module.exports = {
     })
   ],
   devServer: {
+    host: '0.0.0.0',
+    port: 8080,
     static: './dist',
     historyApiFallback: true
   }
