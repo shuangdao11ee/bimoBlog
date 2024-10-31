573bbac8f8f3c32db27d5d82207742812eccd1b5
iAmBimo <shuangdao11ee@outlook.com>
2024-10-31 17:43:09 +0800

    feat: new sentry dsn

--- packages/sentry/src/index.ts
+++ packages/sentry/src/index.ts
@@ -3,23 +3,9 @@ import * as Sentry from '@sentry/react';
 import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom';
 
 export const SentryInit = () => {
-  console.log('[dev] SentryInit');
-
   Sentry.init({
-    dsn: 'http://8f4ba4ea80d9fbabe59a6c5169c33d1f@localhost:9000/2',
-    integrations: [
-      Sentry.browserTracingIntegration(),
-      Sentry.replayIntegration(),
-      // See docs for support of different versions of variation of react router
-      // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-      Sentry.reactRouterV6BrowserTracingIntegration({
-        useEffect,
-        useLocation,
-        useNavigationType,
-        createRoutesFromChildren,
-        matchRoutes
-      })
-    ],
+    dsn: 'https://f4dbcabf942e2bfed0c7475db30d76ec@o4508216688181248.ingest.us.sentry.io/4508216690016256',
+    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
     // Tracing
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
